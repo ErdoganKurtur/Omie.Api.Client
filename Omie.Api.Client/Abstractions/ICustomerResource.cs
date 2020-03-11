db38bd681099a9f82fc3ad01471844e2bae85d65
Felipe silva baltazar <felipe.baltazar@simix.com.br>
2020-03-11 10:54:11 -0300

    Code review 🐛

--- Omie.Api.Client/Abstractions/ICustomerResource.cs
+++ Omie.Api.Client/Abstractions/ICustomerResource.cs
@@ -1,8 +1,9 @@
 ﻿using Omie.Api.Client.Models;
 using Omie.Api.Client.Request;
+using Omie.Api.Client.Request.Customer;
 
 namespace Omie.Api.Client.Abstractions {
     /// <remarks/>
-    public interface ICustomerResource : IBaseResource<Customer, ApiGetManyResult> {
+    public interface ICustomerResource : IBaseResource<Customer, ApiGetManyResult, InsertResult> {
     }
 }
